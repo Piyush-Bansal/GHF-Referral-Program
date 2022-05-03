ea4ec4783e2193399667b29147d5a39a8b93a342
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-05-03 18:10:43 +0530

    send email call webhook

--- airtable data/send emails.js
+++ airtable data/send emails.js
@@ -4,7 +4,7 @@ const messageTag = document.querySelector("#email");
 const sendBtn = document.querySelector("#sendBttn");
 
 //webhook address
-const url = "https://hook.us1.make.com/9bjsg4zukmh6gxd1ogihei7fq7vj69u1";
+const webhookUrl = "https://hook.us1.make.com/9bjsg4zukmh6gxd1ogihei7fq7vj69u1";
 
 //add event listner to the submit button
 sendBtn.addEventListener("click", function (e) {
@@ -12,5 +12,14 @@ sendBtn.addEventListener("click", function (e) {
   let formResponse = new FormData();
   const sendTo = sendToTag.value;
   const message = messageTag.value;
-  console.log(sendTo, message);
+
+  //send data to the webhook
+  formResponse.append("sendto", sendTo);
+  formResponse.append("message", message);
+  fetch(webhookUrl, {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 });
