afec425eece86e8781e6567c9163a99b062c51ab
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-05-03 19:00:55 +0530

    testing

--- airtable data/send emails.js
+++ airtable data/send emails.js
@@ -11,7 +11,7 @@ const webhookUrl = "https://hook.us1.make.com/9bjsg4zukmh6gxd1ogihei7fq7vj69u1";
 //add event listner to the submit button
 sendBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  sendBtn.innerHTML = "Sending...";
+  sendBtn.value = "Sending...";
   //   let formResponse = new FormData();
   //   const sendTo = sendToTag.value;
   //   const message = messageTag.value;
