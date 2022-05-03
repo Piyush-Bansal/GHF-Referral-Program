c2836abee12c111f3294787be623886a2822692c
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2022-05-03 15:56:03 +0530

    error printing

--- airtable data/load data.js
+++ airtable data/load data.js
@@ -2,7 +2,7 @@
 const queryString = window.location.search;
 const urlPara = new URLSearchParams(queryString);
 const refCode = urlPara.get("uuid");
-console.log(uuid);
+console.log(refCode);
 
 //grab loader element data
 const loaderContainer = document.querySelector(".ref_loader");
